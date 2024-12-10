ffe343b887bba006c35043a772cdb5378ac2ff62
Jairaj <pandeyjairaj6299@gmail.com>
2024-12-10 12:32:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// document.querySelector('.join-btn').addEventListener('click', () => {
+//     alert('Join Now button clicked!');
+// });
+
+// document.querySelector('.explore-btn').addEventListener('click', () => {
+//     alert('Explore button clicked!');
+// });
